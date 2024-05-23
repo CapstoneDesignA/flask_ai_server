76ecc557f7e4fadfe0db9d8f6dec97394e4d2fb4
DongchannN <ldc991104@gmail.com>
2024-05-23 12:56:59 +0900

    feat : 예상 매출액을 예상 혼잡도로 반환하는 기능 추가. 기존 Client로 반환하는 값은 예상 매출액이였음. .csv파일에서 'PAYMENT' 컬럼을 모두 읽어 평균과 최대 매출을 구한 뒤 평균 매출액을 혼잡도 40(변경 가능)으로 기준을 두어 선형 방정식을 통하여 혼잡도 반환.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 import datetime
 
@@ -16,12 +17,39 @@ def toFailureResponse(msg):
     response = {'isSuccess': False, 'message': msg}
     return response
 
+
 def toSuccessResponse(data):
     response = {'isSuccess': True, 'data': data}
     return response
+
+
+def calculate_congestion(prediction, average_sales, max_sales, average_congestion=40):
+    if prediction <= 0:
+        return 0
+    elif prediction == average_sales:
+        return average_congestion
+    else:
+        if prediction >= max_sales:
+            return 100
+        else:
+            return average_congestion + ((prediction - average_sales)
+                                         / (max_sales - average_sales)) * (100 - average_congestion)
+
+
+def get_max_and_mean_csv(csv_file_path, column_name):
+    df = pd.read_csv(csv_file_path)
+
+    if column_name not in df.columns:
+        raise ValueError("Non Existent Column")
+    max_value = df[column_name].max()
+    average_value = df[column_name].mean()
+
+    return max_value, average_value
+
 @app.route('/test')
 def index():
     return 'test'
+
 
 @app.route('/test/model')
 def restful_model_predict_test():
@@ -42,16 +70,20 @@ def restful_model_predict_test():
     hour = time_info.hour
     dow = time_info.weekday() + 1
 
-    search_volume = 100 # todo : 검색량은 Flask Server에서 계산하여 넣기.
+    search_volume = 100  # todo : 검색량은 Flask Server에서 계산하여 넣기.
     new_input = np.array([[month, day, dow, hour, rain_percent, search_volume]])
     new_input = new_input.astype('float32')
     prediction = test_model.predict(new_input)
-    ret = str(prediction[0][0])
 
-    # todo : 현재 prediction의 값은 예상 매출이므로 혼잡도로 계산한 뒤 Spring boot Server로 반환하는 작업 필요함.
+    path = 'data/gf_data_test.csv'
+    column_name = 'PAYMENT'
+    max_sales, avg_sales = get_max_and_mean_csv(path, column_name)
+    congestion = calculate_congestion(prediction, avg_sales, max_sales)
+    ret = str(congestion)
     data = {'congestion': ret}
     res = toSuccessResponse(data)
     return res, 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
