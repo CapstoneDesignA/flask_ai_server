83eaca99d8dad994bad6fafa24de0da681f44ae2
DongchannN <ldc991104@gmail.com>
2024-05-30 23:45:46 +0900

    feat : 실시간 검색량 추산 기능 추가. - `/test/model` 요청 시 : 기존에 예상 실시간 검색량을 100으로 하드코딩으로 작성된 것을 최근 28일에 대한 시간대별 평균 검색량에 최근 7일에 대한 요일별 검색량 추이로 오늘의 시간대별 검색량을 추산한 값으로 개선.
    - get_grandfa_factory_trends_google() : pytrends 오픈소스를 이용하여 구글 트렌드의 최근 상대 검색량 조회 시도 -> Too Many Request 에러로 인하여 프록시 설정 등 시간적 비용 문제 발생하여 네이버 데이터랩의 검색어 트랜드로 대체.
    
    - get_grandfa_factory_trends_naver() : 특정 카페의 최근 7일에 대한 상대 검색량 조회. 요일별 상대검색량에 대한 Data Frame으로 반환.
    
    - calculate_mean_search_vol_per_time(csv_file_path) : csv의 파일 경로를 파라미터로 입력 시 최근 28일의 데이터에 대해 시간대별 평균 검색량을 Data Frame으로 반환.
    
    - get_expected_search_vol(target) : 미리 정해진 가게들에 대한 정수값을 파라미터로 입력 시 해당 가게의 시간대별 예상 검색량을 Data Frame으로 반환. 만약 파라미터로 입력되는 정수값이 가게와 매핑되지 않는다면 ValueError를 발생시키고 가게들에 대한 코드값을 출력.
    
    - time_to_period(hour) : 검색량을 시간대별로 나누었기에 시간을 입력 시 시간대를 나타내는 코드를 반환.

--- app.py
+++ app.py
@@ -2,27 +2,94 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import datetime
+import urllib.request
+import json
+import os
 
 from flask import Flask, request
 from flask_restful import Api, Resource
-from tensorflow import keras
+from pytrends.request import TrendReq
 
 app = Flask(__name__)
 api = Api(app)
 
+target_csv_path_list = ['data/gf_data_test.csv']
+client_id = os.getenv('NAVER_CLIENT_ID')
+client_secret = os.getenv('NAVER_CLIENT_SECRET')
+
 test_model = tf.keras.models.load_model('./model/test_sales_prediction_model.h5')
+pytrends = TrendReq(hl='ko', tz=540)
 
 
-def toFailureResponse(msg):
+
+def to_failure_response(msg):
     response = {'isSuccess': False, 'message': msg}
     return response
 
 
-def toSuccessResponse(data):
+def to_success_response(data):
     response = {'isSuccess': True, 'data': data}
     return response
 
 
+# Google trends를 통하여 최근 일주일 상대 검색량 반환. -> pytrends오류로 인한 개발 중지.
+def get_grandfa_factory_trends_google():
+    # https://trends.google.co.kr/trends/explore?date=today%201-m&geo=KR&q=%2Fm%2F043rlyx&hl=ko
+    try:
+        pytrends.build_payload(["성수동2가"], cat=0, timeframe='now 7-d', geo='KR', gprop='')
+    except ValueError as ve:
+        print("구글 트렌드에 검색하기 위한 Value가 잘못되었습니다.")
+    except Exception as e:
+        print("구글 트렌드에서 검색결과를 가져올 수 없습니다.")
+    data = pytrends.interest_over_time()
+
+
+# 네이버 데이터랩을 통하여 최근 7일에 대한 상대 검색량을 반환. MON to SUN 순으로 되어있음.
+def get_grandfa_factory_trends_naver():
+    url = "https://openapi.naver.com/v1/datalab/search";
+    today = datetime.datetime.now()
+    end_date = today - datetime.timedelta(days=1)
+    end_date_str = end_date.strftime('%Y-%m-%d')
+    start_date = today - datetime.timedelta(days=7)
+    start_date_str = start_date.strftime('%Y-%m-%d')
+    body = f"""{{
+        "startDate": "{start_date_str}",
+        "endDate": "{end_date_str}",
+        "timeUnit": "date",
+        "keywordGroups": [
+            {{
+                "groupName": "성수동",
+                "keywords" : ["성수동"]
+            }}
+        ],
+        "device": "mo"
+    }}"""
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id", client_id)
+    request.add_header("X-Naver-Client-Secret", client_secret)
+    request.add_header("Content-Type", "application/json")
+    response = urllib.request.urlopen(request, data=body.encode("utf-8"))
+    rescode = response.getcode()
+    if rescode == 200:
+        response_body = response.read()
+    else:
+        print("Error Code:" + rescode)
+        return
+    parsed_json = json.loads(response_body)
+    data_items = parsed_json['results'][0]['data']
+    df = pd.DataFrame(data_items)
+    df.columns = ['date', 'search_ratio']
+    df['date'] = pd.to_datetime(df['date'])
+    df['weekday'] = df['date'].dt.strftime('%a').str.upper()
+    weekday_mapping = {'MON': 0, 'TUE': 1, 'WED': 2, 'THU': 3, 'FRI': 4, 'SAT': 5, 'SUN': 6}
+    df['weekday_num'] = df['weekday'].map(weekday_mapping)
+    df = df.sort_values('weekday_num').reset_index(drop=True)
+    df = df[['weekday', 'search_ratio']]
+    return df
+
+
+# prediction : 매출, average_sales : 평균 매출, max_sales : 최대 매출, average_congestion : 평균 혼잡도(0~100)
+# 혼잡도를 반환함
 def calculate_congestion(prediction, average_sales, max_sales, average_congestion=40):
     if prediction <= 0:
         return 0
@@ -36,6 +103,7 @@ def calculate_congestion(prediction, average_sales, max_sales, average_congestio
                                          / (max_sales - average_sales)) * (100 - average_congestion)
 
 
+# 데이터 내의 컬럼의 max_value와 mean_value를 구함.
 def get_max_and_mean_csv(csv_file_path, column_name):
     df = pd.read_csv(csv_file_path)
 
@@ -45,6 +113,50 @@ def get_max_and_mean_csv(csv_file_path, column_name):
     average_value = df[column_name].mean()
 
     return max_value, average_value
+
+
+# 최근 28일 데이터를 통하여 각 시간대별 평균 검색량은 반환.
+def calculate_mean_search_vol_per_time(csv_file_path):
+    df = pd.read_csv(csv_file_path)
+    recent_df = df.tail(28)
+    search_vol_data_per_time = recent_df[['TIME', 'SEARCH_VOL']]
+    mean_search_vol_per_time = search_vol_data_per_time.groupby('TIME')['SEARCH_VOL'].mean()
+
+    return mean_search_vol_per_time
+
+
+# 파라미터로 가게에 대해 정해놓은 인덱스 값을 넣어 시간대별로 기대되는 검색량을 반환함.
+def get_expected_search_vol(target):
+    mean_search_vols_per_time = calculate_mean_search_vol_per_time(target_csv_path_list[target])
+    if target == 1:
+        actual_search_ratio_per_day = get_grandfa_factory_trends_naver()
+    else:
+        raise ValueError("0 : grandfa_factory")
+    mean_search_ratio = actual_search_ratio_per_day['search_ratio'].mean()
+    today_weekday = datetime.datetime.today().weekday()
+    mul = actual_search_ratio_per_day.iloc[today_weekday]['search_ratio'] / mean_search_ratio
+    ret = mean_search_vols_per_time * mul
+
+    return ret
+
+
+# 시간을 구간별로 나누어 1부터 6사이의 정수로 치환
+def time_to_period(hour):
+    if 0 <= hour < 6:
+        return 1
+    elif 6 <= hour < 11:
+        return 2
+    elif 11 <= hour < 14:
+        return 3
+    elif 14 <= hour < 17:
+        return 4
+    elif 17 <= hour < 21:
+        return 5
+    elif 21 <= hour < 24:
+        return 6
+    else:
+        raise ValueError("Hour must be between 0 and 23")
+
 
 @app.route('/test')
 def index():
@@ -57,31 +169,33 @@ def restful_model_predict_test():
     required_params = ['rain_percent']
     for param in required_params:
         if param not in parameter_dict:
-            return toFailureResponse('rain_percent param is necessary.'), 400
+            return to_failure_response('rain_percent param is necessary.'), 400
 
     try:
         rain_percent = float(parameter_dict['rain_percent'])
     except ValueError as e:
-        return toFailureResponse('rain_percent is not numeric.'), 400
+        return to_failure_response('rain_percent is not numeric.'), 400
 
+    # Model 에 입력할 파라미터 준비
     time_info = datetime.datetime.now()
     month = time_info.month
     day = time_info.day
     hour = time_info.hour
+    period = time_to_period(hour)
     dow = time_info.weekday() + 1
+    expected_search_vol = get_expected_search_vol(0) # 0 : 할아버지 공장
+    search_volume = expected_search_vol[period]
 
-    search_volume = 100  # todo : 검색량은 Flask Server에서 계산하여 넣기.
-    new_input = np.array([[month, day, dow, hour, rain_percent, search_volume]])
+    new_input = np.array([[month, day, dow, period, rain_percent, search_volume]])
     new_input = new_input.astype('float32')
     prediction = test_model.predict(new_input)
 
-    path = 'data/gf_data_test.csv'
     column_name = 'PAYMENT'
-    max_sales, avg_sales = get_max_and_mean_csv(path, column_name)
+    max_sales, avg_sales = get_max_and_mean_csv(target_csv_path_list[0], column_name)
     congestion = calculate_congestion(prediction[0], avg_sales, max_sales)
     ret = str(congestion[0])
     data = {'congestion': ret}
-    res = toSuccessResponse(data)
+    res = to_success_response(data)
     return res, 200
 
 
